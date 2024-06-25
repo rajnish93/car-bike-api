bf2bfb65e96dcf66089ac9761727c55756ddb92c
Rajnish Singh <10815566+rajnish93@users.noreply.github.com>
2024-06-26 00:44:57 +0530

    fix: auth and create-user dto

--- src/modules/users/dto/auth-user.dto.ts
+++ src/modules/users/dto/auth-user.dto.ts
@@ -1,4 +1,22 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
-export class AuthUserDto extends PartialType(CreateUserDto) {}
+export class AuthUserDto {
+  @ApiProperty({
+    example: 'rajnish@example.com',
+    description: 'The email of the User',
+  })
+  @IsEmail()
+  @Transform(({ value }) => value?.toLowerCase()?.trim())
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({
+    example: 'password@123',
+    description: 'The password of the User',
+  })
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -9,19 +9,28 @@ import {
 } from 'class-validator';
 
 export class CreateUserDto {
-  @ApiProperty()
+  @ApiProperty({
+    example: 'Rajnish Singh',
+    description: 'The name of the User',
+  })
   @IsString()
   @Transform(({ value }) => value?.trim())
   @IsNotEmpty()
   name: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'rajnish@example.com',
+    description: 'The email of the User',
+  })
   @IsEmail()
   @Transform(({ value }) => value?.toLowerCase()?.trim())
   @IsNotEmpty()
   email: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'password@123',
+    description: 'The password of the User',
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(5, { message: 'Password should contain more than 5 letters' })
